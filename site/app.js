e1f3443f388aefbd78d4eb0ee04d8914aa3b55e8
Dana Gutride <dgutride@redhat.com>
2019-07-08 16:08:10 -0400

    Add site files for building zip

---
+++ site/app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var es6Renderer = require('express-es6-template-engine');
+var app = express();
+
+// view engine setup
+app.engine('html', es6Renderer);
+app.set('views', path.join(__dirname, '.'));
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  // res.header('Expires', '-1');
+  // res.header('Pragma', 'no-cache');
+  res.render('server/template', {
+    locals: {
+      title:  'PatternFly'
+    }
+  });
+});
+
+app.get('/iframe', function(req, res) {
+  // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  // res.header('Expires', '-1');
+  // res.header('Pragma', 'no-cache');
+  res.render('server/iframe', {
+    locals: {
+      title:  'PatternFly'
+    },
+    partials: {
+      partial: '/layout'
+    }
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('server/views/error');
+});
+
+module.exports = app;
---
+++ site/build-zip.sh
@@ -0,0 +1,1 @@
+tar --exclude=.git/ --exclude=.idea/ --exclude=build-zip.sh --exclude=.DS_Store --exclude=node_modules/ -zcvf pf-express.tar.gz .
