cba33129f15ec27f137437bce3cb8bec0a85106a
Dana Gutride <dgutride@redhat.com>
2019-07-09 09:46:19 -0400

    Add css file to layout

--- site/app.js
+++ site/app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 app.get('/', function(req, res) {
   // res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
