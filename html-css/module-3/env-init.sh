a797cda016869deb45659f95d1694abe6f85ff95
Dana Gutride <dgutride@gmail.com>
2019-07-09 16:23:34 -0400

    chore(init): modify file path for downloading base

--- html-css/module-3/env-init.sh
+++ html-css/module-3/env-init.sh
@@ -1,4 +1,4 @@
-curl -L https://www.dropbox.com/s/u1ofp8ch795zkvr/pf-express.tar.gz?dl=0 > ~/pf-express.tar.gz
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/site/pf-express.tar.gz > ~/pf-express.tar.gz
 tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
