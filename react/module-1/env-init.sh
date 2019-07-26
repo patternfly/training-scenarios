4e756932f8a3d0f383f5a3cd800c8774e6a62fda
Dana Gutride <dgutride@gmail.com>
2019-07-26 08:51:53 -0400

    Add react-site (#38)
    * Initial commit for react-site files
    
    * Add module to pathway
    
    * Use cra instead of seed
    
    * chore(cleanup): cleanup initial module and script

---
+++ react/module-1/env-init.sh
@@ -0,0 +1,6 @@
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react-site/pf-react-express.tar.gz > ~/pf-express.tar.gz
+tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
+cd ~/tutorial
+npm install
+echo "Starting... this will block the rest of the commands from running..."
+npm start
