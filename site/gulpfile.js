3a5cd6899b6ba876a38be14ba75a9084d3fe9c72
jenny-s51 <jshandel12@gmail.com>
2019-07-31 11:29:32 -0400

    rebase with master

---
+++ site/gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function() {
+    return gulp.src('.scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+});
+
+// compile scss into css
+function style () {
+    return gulp.src('./scss/**/*.scss').pipe(sass()).pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+}
+
+function watch () {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+
+}
+exports.style = style;
+exports.watch = watch;
