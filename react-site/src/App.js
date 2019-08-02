99225cda81730b59da769d6657609c308117cf66
Dana Gutride <dgutride@gmail.com>
2019-08-02 12:51:47 -0400

    Change polling model and add hmr

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import '@patternfly/react-core/dist/styles/base.css';
+import "@patternfly/react-core/dist/styles/base.css";
+import {
+  Brand,
+  Button,
+  Card,
+  CardActions,
+  CardHead,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Split,
+  SplitItem,
+  Text,
+  TextContent,
+  TextVariants
+} from '@patternfly/react-core';
+import {
+  TimesIcon
+} from '@patternfly/react-icons';
 
 function App() {
   return (
-    <div> Welcome to PatternFly </div>
-  );
+      <Card>
+      <CardHead>
+      <CardActions>
+      </CardActions>
+      </CardHead>
+      <CardHeader>
+      </CardHeader>
+      <CardBody>
+      </CardBody>
+      <CardFooter>
+      </CardFooter>
+      </Card>
+);
 }
 
 export default App;
--- react/module-1/env-init.sh
+++ react/module-1/env-init.sh
@@ -2,5 +2,6 @@ curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/r
 tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
+export CHOKIDAR_USEPOLLING=true
 echo "Starting... this will block the rest of the commands from running..."
 npm start
