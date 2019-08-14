b3fb65e8ef9b3032690de05dd1182abed1311454
Dana Gutride <dgutride@gmail.com>
2019-08-14 16:23:47 -0400

    Move init to curl

--- react/module-2/env-init.sh
+++ react/module-2/env-init.sh
@@ -3,6 +3,9 @@ tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
 export CHOKIDAR_USEPOLLING=true
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/header.js > ~/tutorial/src/components/header.js
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/page.js > ~/tutorial/src/components/page.js
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/toolbar.js > ~/tutorial/src/components/toolbar.js
 touch ./src/app.css
 echo "Starting... this will block the rest of the commands from running..."
 npm start
