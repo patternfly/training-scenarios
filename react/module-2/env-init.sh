81c820a7cc1ee5f243dda2c7ae2c0d8ba0dfd441
Dana Gutride <dgutride@gmail.com>
2019-08-14 16:52:35 -0400

    Modify init

--- react/module-2/env-init.sh
+++ react/module-2/env-init.sh
@@ -4,9 +4,9 @@ cd ~/tutorial
 npm install
 export CHOKIDAR_USEPOLLING=true
 mkdir ~/tutorial/src/components
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/header.js > ~/tutorial/src/components/header.js
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/page.js > ~/tutorial/src/components/page.js
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/assets/toolbar.js > ~/tutorial/src/components/toolbar.js
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/module-2/assets/header.js > ~/tutorial/src/components/header.js
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/module-2/assets/page.js > ~/tutorial/src/components/page.js
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react/module-2/assets/toolbar.js > ~/tutorial/src/components/toolbar.js
 touch ./src/app.css
 echo "Starting... this will block the rest of the commands from running..."
 npm start
