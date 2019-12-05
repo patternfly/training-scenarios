921876a19a25c73a5306393498359b687cee9d0a
Katie McFaul <kmcfaul@redhat.com>
2019-12-05 14:07:52 -0500

    add react-core install

--- react-components/select/env-init.sh
+++ react-components/select/env-init.sh
@@ -2,6 +2,7 @@ curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/r
 tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
+npm install @patternfly/react-core
 export CHOKIDAR_USEPOLLING=true
 touch ./src/data.js
 echo "Starting... this will block the rest of the commands from running..."
