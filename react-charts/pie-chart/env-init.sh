5eabf6dd4f2273dd81a119f568750727efc6d319
Christie Molloy <cmolloy@redhat.com>
2020-06-22 10:37:36 -0400

    update init script (#292)

--- react-charts/pie-chart/env-init.sh
+++ react-charts/pie-chart/env-init.sh
@@ -1,6 +1,5 @@
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react-site/pf-react-express.tar.gz > ~/pf-express.tar.gz
-tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
-cd ~/tutorial
+curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react-site/pf-react-express.tar.gz > /root/pf-express.tar.gz
+tar -xvzf /root/pf-express.tar.gz
 npm install
 npm install @patternfly/react-tokens@prerelease
 npm install @patternfly/react-charts@prerelease
