b9499fe2cf491814be21d6c2678079005cc58b66
Joachim Schuler <jschuler@redhat.com>
2020-07-29 14:03:34 -0400

    update PF and regenerate lock file

--- react-site/build-zip.sh
+++ react-site/build-zip.sh
@@ -1,1 +1,1 @@
-tar --exclude=.git/ --exclude=.idea/ --exclude=build-zip.sh --exclude=.DS_Store --exclude=node_modules/ --exclude=css/ -zcvf pf-react-express.tar.gz .
+tar --exclude=.git/ --exclude=.idea/ --exclude=build-zip.sh --exclude=.DS_Store --exclude=node_modules/ --exclude=css/ --exclude=pf-react-express.tar.gz -zcvf pf-react-express.tar.gz .
